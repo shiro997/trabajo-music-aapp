e2560495fbacf77990b8d3f941e5f6a65195198e
Wilmer Angarita <willy3230l@gmail.com>
2019-10-14 22:03:29 -0500

    Add artist list activity, modify songs activity

---
+++ android-cardview/app/src/main/java/co/edu/konradlorenz/cardview/ArtistActivity.java
@@ -0,0 +1,13 @@
+package co.edu.konradlorenz.cardview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArtistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist);
+    }
+}
