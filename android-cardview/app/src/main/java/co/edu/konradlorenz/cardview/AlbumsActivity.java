790336f61a8a506e666ad9fda4c8448cdee37945
Wilmer Angarita <willy3230l@gmail.com>
2019-10-15 23:26:03 -0500

    Add listview java code (artist and albums)
    Add listview java code (artist and albums)

--- android-cardview/app/src/main/java/co/edu/konradlorenz/cardview/AlbumsActivity.java
+++ android-cardview/app/src/main/java/co/edu/konradlorenz/cardview/AlbumsActivity.java
@@ -2,12 +2,23 @@ package co.edu.konradlorenz.cardview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AlbumsActivity extends AppCompatActivity {
-
+    private ArrayList<String> albumList;
+    private ArrayAdapter<String> neoAdapter;
+    private ListView neoListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_albums);
+        //Adding artist lists
+        neoListView=(ListView)findViewById(R.id.lviAlbumList);
+        String[] val= new String[]{"Rock","Clasico","Pop"};
+        neoAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,val);
+        neoListView.setAdapter(neoAdapter);
     }
 }
--- android-cardview/app/src/main/java/co/edu/konradlorenz/cardview/ArtistActivity.java
+++ android-cardview/app/src/main/java/co/edu/konradlorenz/cardview/ArtistActivity.java
@@ -2,12 +2,23 @@ package co.edu.konradlorenz.cardview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ArtistActivity extends AppCompatActivity {
-
+    private ArrayList<String> artistList;
+    private ArrayAdapter<String> neoAdapter;
+    private ListView neoListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist);
+        //Adding artist lists
+        neoListView=(ListView)findViewById(R.id.lviArtistList);
+        String[] val= new String[]{"Greenday","Aerosmith","Metallica"};
+        neoAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,val);
+        neoListView.setAdapter(neoAdapter);
     }
 }
